bb85a6dc9c3857949ad3b9a37890e6326e8e60cb
HARSHIT PRASAD <barixc98@gmail.com>
2025-02-17 22:13:32 +0530

    Fix theme provider types and add Netlify config

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,6 +1,11 @@
 "use client"
+
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import type { ThemeProviderProps } from "next-themes"
+import type { ThemeProviderProps as NextThemeProviderProps } from "next-themes"
+
+interface ThemeProviderProps extends Omit<NextThemeProviderProps, 'children'> {
+  children: React.ReactNode
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
