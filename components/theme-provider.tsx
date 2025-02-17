b6b4303a7eb8895941cfadecb2a7aae4b82e2265
HARSHIT PRASAD <barixc98@gmail.com>
2025-02-17 22:18:59 +0530

    Simplify theme provider and update configuration

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,13 +1,19 @@
 "use client"
 
+import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import type { ThemeProviderProps as NextThemeProviderProps } from "next-themes"
 
-interface ThemeProviderProps extends Omit<NextThemeProviderProps, 'children'> {
-  children: React.ReactNode
-}
-
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+export function ThemeProvider({ children, ...props }: { children: React.ReactNode }) {
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+      {...props}
+    >
+      {children}
+    </NextThemesProvider>
+  )
 }
 
